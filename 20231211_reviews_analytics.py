d92da4c9998c48c030eccb3e5a6f3ae909293a23
Milly <wmr4job@gmail.com>
2023-12-11 19:08:06 +0800

    calculate average

--- 20231211_reviews_analytics.py
+++ 20231211_reviews_analytics.py
@@ -1,11 +1,22 @@
 #reviews-analytics 留言分析
 
-data = []
-count = 0
+#每讀取1000筆就印出一次
+# data = []
+# count = 0
+# with open('reviews.txt', 'r') as f: #讀取reviews檔
+# 	for line in f:
+# 		data.append(line)
+# 		count += 1
+# 		if count % 1000 == 0: # %是取餘數的運算符號，每1000筆印出一次
+# 			print(len(data))
+# print('檔案讀取完畢，總共有', len(data), '筆資料')
+
+#計算每筆留言平均長度
+sum_len = 0
+data_len = []
 with open('reviews.txt', 'r') as f: #讀取reviews檔
 	for line in f:
-		data.append(line)
-		count += 1
-		if count % 1000 == 0: # %是取餘數的運算符號，每1000筆印出一次
-			print(len(data))
-print('檔案讀取完畢，總共有', len(data), '筆資料')
+		data_len.append(len(line))
+		sum_len += len(line)
+average = sum_len / len(data_len)
+print('每筆留言平均長度：',average)
